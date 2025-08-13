69852588686e58a78b53032c1a64142b34969e82
developerwk <wasifulkabir2023@gmail.com>
2025-08-14 00:13:49 +0600

    Add rate limiting and update API configuration

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import cors from "cors";
 import express from "express";
+import rateLimit from "express-rate-limit";
 import morgan from "morgan";
 import errorHandler from "./middlewares/errorHandler";
 import notFoundMiddleware from "./middlewares/notFoundMiddleware";
 import businessOrderRoutes from "./routes/BusinessOrderRoutes";
 import cashAccountRoutes from "./routes/cashAccountRoutes";
-import cashFlowRoutes from "./routes/cashTransactionRoutes";
+import cashTransactionRoutes from "./routes/cashTransactionRoutes";
 import categoryRoutes from "./routes/categoryRoutes";
 import orderRoutes from "./routes/orderRoutes";
 import productRoutes from "./routes/productRoutes";
@@ -13,14 +14,21 @@ import supplierRoutes from "./routes/supplierRoutes";
 import totalCountRoute from "./routes/totalCountRoute";
 import userRoutes from "./routes/userRoutes";
 const app = express();
-
-// Middleware
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again later.",
+  validate: true,
+  standardHeaders: true,
+});
+app.use(limiter);
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.CLIENT_URI,
     methods: ["GET", "POST", "PATCH", "DELETE"],
   }),
 );
+
 app.use(express.json());
 
 app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
@@ -30,11 +38,11 @@ app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/categories", categoryRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/orders", orderRoutes);
-app.use("/api/v1/cash-transactions", cashFlowRoutes);
-app.use("/api/v1/cash-account", cashAccountRoutes);
+app.use("/api/v1/cash-transactions", cashTransactionRoutes);
+app.use("/api/v1/cash-accounts", cashAccountRoutes);
 app.use("/api/v1/suppliers", supplierRoutes);
 app.use("/api/v1/total-counts", totalCountRoute);
-app.use("/api/v1/business-orders", businessOrderRoutes);
+app.use("/api/v1/b2b-orders", businessOrderRoutes);
 
 // Middleware
 app.use(notFoundMiddleware);
--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,23 +1,3 @@
-// import express, { Router } from "express";
-// import {
-//   createProduct,
-//   deleteProduct,
-//   getProduct,
-//   getProducts,
-//   updateProduct,
-// } from "../controllers/productController";
-// import isAdminMiddleware from "../middlewares/adminMiddleware";
-// import authMiddleware from "../middlewares/authMiddleware";
-
-// const router: Router = express.Router();
-
-// router.post("", [authMiddleware, isAdminMiddleware], createProduct);
-// router.get("", getProducts);
-// router.get("/:id", getProduct);
-// router.patch("/:id", [authMiddleware, isAdminMiddleware], updateProduct);
-// router.delete("/:id", [authMiddleware, isAdminMiddleware], deleteProduct);
-
-// export default router;
 import { Router } from "express";
 import {
   createProduct,
